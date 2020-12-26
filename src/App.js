47bf07d94ea6a4714e4f382d7955c66f90c4f847
Erin Zimmer <ejzimmer@gmail.com>
2020-12-26 17:57:40 +1100

    save to storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
+
 import './App.css';
 
+const STORAGE_KEY = 'tasks'
+
 function App() {
+  const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState(() => {
+    const value = localStorage.getItem(STORAGE_KEY)
+    return value ? JSON.parse(value) : []
+  })
+
+  useEffect(() => {
+    if (tasks.length > 0)
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks))
+  }, [tasks])
+
+  const updateTaskDescription = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const addTask = (event) => {
+    if (event.key == 'Enter' && !event.shiftKey) {
+      const newTask = {
+        id: (new Date()).getTime(),  
+        description: event.target.value
+      }
+
+      setTasks([...tasks, newTask])
+      setNewTask('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className="day-list">
+      {tasks.map(task => <li key={task.id} className="task">{task.description}</li>)}
+      <li style={{ height: '100%' }}>
+        <textarea 
+          onChange={updateTaskDescription} 
+          value={newTask} 
+          onKeyDown={addTask} 
+          data-testid="new-task" 
+          rows={1} 
+          className="new-task" />
+      </li>
+    </ul>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,61 @@
-import { render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    setItem: jest.fn(),
+    getItem: () => JSON.stringify([{
+      id: 1,
+      description: 'Wash dishes'
+    }, {
+      id: 2,
+      description: 'Mow lawn'
+    }])
+  }
+})
+
+function createCard() {
+  act(() => {
+    const input = screen.getByTestId('new-task')
+    fireEvent.keyDown(input, { 
+      key: 'Enter',
+      shiftKey: false,
+      target: {
+        value: 'Buy milk'
+      }
+    })
+  })
+}
+
+const getTasks = () => screen.getAllByRole('listitem')
+
+describe('the list', () => {
+  beforeEach(() => {
+    render(<App />)
+  })
+
+  it('can add an item to the list', async () => {
+    createCard()
+
+    const tasks = getTasks()
+    expect(tasks[tasks.length - 2]).toHaveTextContent('Buy milk')
+    expect(tasks[tasks.length - 1].querySelector('textarea').value).toBe('')  
+  })
+
+  it('saves in localstorage', () => {
+    const setItem = jest.spyOn(localStorage, 'setItem')
+    setItem.mockClear()
+
+    createCard()
+
+    const [key, value] = setItem.mock.calls[0]
+    expect(key).toBe('tasks')
+    expect(value).toContain('Buy milk')
+  })
+
+  it('fetches initial state from localstorage', () => {
+    const tasks = getTasks()
+    expect(tasks.length).toBe(3) // 2 tasks + new task card
+  })
+})
