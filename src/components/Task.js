c285bfe61b5cc8ea8932a9d7c59f45f2ea7dfa56
Erin Zimmer <ejzimmer@gmail.com>
2021-01-02 08:28:29 +1100

    edit descriptions

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import './Task.css'
 
 export default function Task({task, deleteTask, updateTask}) {
+  const [editMode, setEditMode] = useState(false)
+  const editor = useRef()
+
+  useEffect(() => {
+    if (editMode && editor.current)
+      editor.current.focus()
+  }, [editMode])
+
   function setTaskDone() {
     const done = !task.done
     updateTask({
@@ -11,10 +19,19 @@ export default function Task({task, deleteTask, updateTask}) {
     })
   }
 
+  function updateTaskDescription(event) {
+    const description = event.target.value
+    updateTask({
+      ...task,
+      description
+    })
+  }
+
   return (
     <div className="task">
       <input id={`task_${task.id}`} type="checkbox" checked={!!task.done} onChange={setTaskDone} />
-      <label htmlFor={`task_${task.id}`}>{task.description}</label>
+      { !editMode && <label htmlFor={`task_${task.id}`}>{task.description}</label>}
+      { editMode && <input type="text" ref={editor} value={task.description} onChange={updateTaskDescription} onBlur={() => setEditMode(false)} />}
 
       <button 
         aria-label={`delete task ${task.description}`} 
@@ -24,7 +41,14 @@ export default function Task({task, deleteTask, updateTask}) {
           <path d="M0,0 l100,100" />
           <path d="M100,0 l-100,100" />
         </svg>
-        </button>
+      </button>
+      { !task.done && (
+        <button className="edit" aria-label={`edit task ${task.description}`} onClick={() => setEditMode(true)}>
+        <svg width="100%" viewBox="0 0 100 100">
+          <path d="M0,80 l0,20 20,0z" />
+          <path d="M9,69 l20,20 65,-65 -20,-20" />
+        </svg>
+        </button>)}
     </div>
   )
 }
